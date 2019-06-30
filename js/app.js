7702ad9b8489dcebb5bf61b426d042be473e15ec
Thierry Legros <thierry.legros2@club.fr>
2019-06-30 18:39:16 +0200

    affichage du texte + checkbox, ne barre pas le texte...

--- js/app.js
+++ js/app.js
@@ -1,31 +1,30 @@
-const myTaskList = document.getElementsByTagName("LI");
-let i;
-for (i = 0; i < myTaskList.length; i++) {
-    const button = document.createElement("button");
-    const btn = document.createTextNode("x");
-    button.className = "close";
-    button.appendChild(btn);
-    myTaskList[i].appendChild(button);
-}
-
-const close = document.getElementsByClassName("close");
-let j;
-for (j = 0; j < close.length; j++) {
-    close[j].onclick = function() {
-        const div = this.parentElement;
-        div.style.display = "none";
-    }
-}
-
-function NewAdd(entree) {
+function NewAdd() {
     const li = document.createElement('li');
     const checkbox = document.createElement('input');
     const todo = document.getElementById('entree').value;
-    // const txt = todo.innerText;
+    const myTaskList = document.getElementsByTagName("li");
+    let i;
+    const close = document.getElementsByClassName("close");
+    let j;
 
     listeActuelle.appendChild(li);
     checkbox.type = 'checkbox';
     li.textContent = todo;
     li.appendChild(checkbox);
+
+    for (i = 0; i < myTaskList.length; i++) {
+        const button = document.createElement("button");
+        const btn = document.createTextNode("x");
+        button.className = "close";
+        button.appendChild(btn);
+        myTaskList[i].appendChild(button);
+    }
+
+    for (j = 0; j < close.length; j++) {
+        close[j].onclick = function() {
+            const div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
 
 }
